ab41e8065365c05af7ffba1729853fea3dc3158b
pebbly <59623949+TheOneWhoPebs@users.noreply.github.com>
2024-01-15 17:18:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import os
 import time
-local_ip = "insert ip here"
-scrcpy_folder="insert scrcpy folder here"
+local_ip = "insert local ip here"
+scrcpy_folder="insert path to scrcpy folder here"
 port = input("Port: ")
-os.system(f"cd {scrcpy_folder}")
-os.system(f".\scrcpy --tcpip={local_ip}:{port}")
+os.system(f"cd {scrcpy_folder} &&" + f".\scrcpy --tcpip={local_ip}:{port}")
 time.sleep(2)
 exit()
